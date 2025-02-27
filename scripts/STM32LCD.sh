4cd80a823edcc9b1e322e05a111aff80c483a7ff
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-02-27 08:47:15 +0800

    fix help info

--- scripts/STM32LCD.sh
+++ scripts/STM32LCD.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then
+       echo \
+'
+Usage: '$0' [Option]
+Options:
+        (  Gitee|Github main|dev|<branch> --force  ) |
+        (  Gitee|Github main|dev|<branch>  )         |
+        (  Gitee|Github  )
+
+        Gitee|Github             Repo mirror to download resource [default: Gitee]
+        main|dev|<branch>        Repo branch where download resouce from [default: main]
+        --force                  Change judge system kernel to pass if avaliable
+'
+
+       exit 1
+fi
+
+
 DownloadSite=${1:-Gitee}
 Brach=${2:-main}
 
