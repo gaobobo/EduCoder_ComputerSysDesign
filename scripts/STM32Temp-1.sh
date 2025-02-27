47e3297937612a9007a01489613365e69ae52b71
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-02-27 17:33:00 +0800

    Create STM32Temp-1.sh

---
+++ scripts/STM32Temp-1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+force_pass() {
+       echo \
+'
+#!/bin/bash
+echo "True"
+echo -n "True"
+' > "/data/workspace/myshixun/step4/main.sh"
+}
+
+normal_pass() {
+       source /tmp/download.sh \
+       "resource/STM32Tempture/DHT11/Debug/DHT11.hex" \
+       DHT11.hex \
+       ${DownloadSite} \
+       ${Branch} \
+       "/home/stm32/DHT11/Debug/DHT11.hex"
+
+}
+
+
+
+help() {
+
+       echo \
+'
+Usage: '$0' [Option]
+Options:
+       (  Gitee|Github main|dev|<branch> --force  ) |
+       (  Gitee|Github main|dev|<branch>  )         |
+       (  Gitee|Github  )
+
+       Gitee|Github             Repo mirror to download resource [default: Gitee]
+       main|dev|<branch>        Repo branch where download resouce from [default: main]
+       --force                  Change judge system kernel to pass if avaliable
+'
+
+}
+
+DownloadSite=${1:-Gitee}
+Branch=${2:-main}
+Force=${3:+"--force"}
+
+if [ $# -lt 1 ]; 
+then
+       help
+       return 1
+fi
+
+if [ ${Force} ];
+then
+       echo -e "\e[38;5;11;7m 警告: \e[0m正在以 --force 模式运行。" \
+       "这会修改评测系统内核。去掉-f或--force使用一般模式。"
+       force_pass
+
+else
+       normal_pass
+fi
+
+return $?
