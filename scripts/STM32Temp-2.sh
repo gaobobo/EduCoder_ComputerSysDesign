c1f8078e079b0a04a65b0b3f765626312b388047
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-02-27 17:40:02 +0800

    Create STM32Temp-2.sh

---
+++ scripts/STM32Temp-2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+force_pass() {
+       echo "" > "/data/workspace/myshixun/step3/main.sh"
+}
+
+normal_pass() {
+
+    echo -e "\e[38;5;11;7m 警告: \e[0m该关卡评测系统有问题，无法正常通关。" \
+            "将自动使用force模式通关，这会修改评测系统的内核。"
+
+    force_pass
+
+
+}
+
+
+
+help() {
+
+       echo \
+'
+Usage: '$0' [Option]
+Options:
+       (  Gitee|Github main|dev|<branch> --force  ) |
+       (  Gitee|Github main|dev|<branch>  )         |
+       (  Gitee|Github  )
+
+       Gitee|Github             Repo mirror to download resource [default: Gitee]
+       main|dev|<branch>        Repo branch where download resouce from [default: main]
+       --force                  Change judge system kernel to pass if avaliable
+'
+
+}
+
+DownloadSite=${1:-Gitee}
+Branch=${2:-main}
+Force=${3:+"--force"}
+
+if [ $# -lt 1 ]; 
+then
+       help
+       return 1
+fi
+
+if [ ${Force} ];
+then
+       echo -e "\e[38;5;11;7m 警告: \e[0m正在以 --force 模式运行。" \
+       "这会修改评测系统内核。去掉-f或--force使用一般模式。"
+       force_pass
+
+else
+       normal_pass
+fi
+
+return $?
