b48781f7a3981c1ada125448e2a48d2e0a7aeafa
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-01 09:11:34 +0800

    Revert "Add README"

--- start.sh
+++ start.sh
@@ -4,19 +4,19 @@ helpInfo(){
 
     echo \
 '
-Usage: '$0' <Level> [-f|--force] [-m|--mirror [Gitee|Github]] [-b|--branch [main|dev|<branch>]]
+Usage: '$0' <Level> [-f|--force] [-m|--mirror [Gitee|Github]] [-b|--Branch [main|dev|<branch>]]
 Options:
         <Level>                              Level Code. See at /scripts/ in repo
         -f|--force                           Change judge system kernel to pass if avaliable
         -m|--mirrior [Gitee|Github]          Repo mirror to download resource [default: Gitee]
-        -b|--branch [main|dev|<branch>]      Repo branch where download resouce from [default: main]
+        -b|--Branch [main|dev|<branch>]       Repo branch where download resouce from [default: main]
 
-用法: '$0' <Level> [-f|--force] [-m|--mirror [Gitee|Github]] [-b|--branch [main|dev|<branch>]]
+用法: '$0' <Level> [-f|--force] [-m|--mirror [Gitee|Github]] [-b|--Branch [main|dev|<branch>]]
 选项:
         <Level>                              关卡代码。详见仓库的/scripts/目录。
         -f|--force                           如可用，修改评测系统内核通关。
         -m|--mirrior [Gitee|Github]          下载资源文件时使用的仓库镜像。 [默认: Gitee]
-        -b|--branch [main|dev|<branch>]      下载资源文件时的分支。 [默认: main]
+        -b|--Branch [main|dev|<branch>]       下载资源文件时的分支。 [默认: main]
 
 ============================================
          EduCoder_ComputerSysDesign
